d74c297a4cfe6050f1da6052baee49b3b3b15e20
BeeIsStr <156618675+esthersinche@users.noreply.github.com>
2025-05-14 16:18:41 -0500

    si

---
+++ demo/src/main/java/com/utp/demo/controller/BarcoController.java
@@ -0,0 +1,17 @@
+package com.utp.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.utp.demo.model.Barcos;
+import com.utp.demo.service.BarcoService;
+
+@Controller
+public class BarcoController {
+
+}
