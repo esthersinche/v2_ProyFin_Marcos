3cd54ce233f065a37949f7ea3dbe015c299973be
Laura <lydiaberry2003@gmail.com>
2025-07-13 21:43:52 -0500

    login funciona causas
    se pudo

--- demo/src/main/java/com/utp/demo/service/UsuarioDetailsService.java
+++ demo/src/main/java/com/utp/demo/service/UsuarioDetailsService.java
@@ -1,16 +1,21 @@
 package com.utp.demo.service;
 
-import com.utp.demo.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.*;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.utp.demo.model.Usuario;
 
 @Service
 public class UsuarioDetailsService implements UserDetailsService {
 
     @Autowired
     private UsuarioRepository repo;
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -21,5 +26,10 @@ public class UsuarioDetailsService implements UserDetailsService {
                 usuario.getPassword(),
                 AuthorityUtils.createAuthorityList("ROLE_" + usuario.getRol())
         );
+        
+
     }
+
+    
+
 }
